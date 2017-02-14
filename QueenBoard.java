f8b28ee41c61526e313f5ab9f8cf08ac12bb5c03
soe43 <soe433@gmail.com>
2017-02-13 21:58:17 -0500

    Working on debugging current solution and logic. Only one Queen showing up on the board and numbers are looking weird when debugging.

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,12 +1,12 @@
 public class QueenBoard{
     private int[][]board;
-    private int solutionCount;
+    private int solutionCount = -1;
 
-    public Queenboard(int size){
+    public QueenBoard(int size){
 	board = new int[size][size];
 	for(int i = 0;i<board.length;i++){
-	    for(int k = 0;k<board[0].length;i++){
-		board[i][k] == 0;
+	    for(int k = 0;k<board.length;k++){
+		board[i][k] = 0;
 	    }
 	}
     }
@@ -18,7 +18,7 @@ public class QueenBoard{
 
     //Method that solves for one possible solution of a given board.
     private boolean solveH(int col){
-	if(col >= board.length){
+	if(col == board.length){
 	    return true;
 	}
 	if(board.length == 2 || board.length == 3){
@@ -26,58 +26,50 @@ public class QueenBoard{
 	}
 	for(int row = 0; row < board.length; row++){
 	    if(board[row][col] == 0){
-		addQueen(row,col);
-		return solveH(col+1);
-	    }
-	    if(row >= board.length){
-		return false;
+		addQueen(col,row);
+		if(solveH(col+1)){
+		    return true;
+		}
+		else{
+		    return false;
+		}
+	    }else{
+		removeQueen(col,row);
 	    }
 	}
-
+	return false;
     }
     
     //Adds a queen to row,column and adds all of the danger markers that it threatens
-    private void addQueen(int row,int col){
-	for(int i = 0;i<board.length;i++){
-	    board[row][i] += 1;
-	    board[i][col] += 1;
-	    if(i < row){
-		if(i < col){
-		    board[row - i][col - i] += 1;
-		}else{
-		    board[row - i][col + i] += 1;
-		}
+    private void addQueen(int col,int row){
+	board[col][row] = -1;
+	for(int i = 0; i<board.length;i++){
+	    if(i != col){
+		board[i][row] += 1;
 	    }
-	    if(i > row){
-		if(i < col){
-		    board[row + i][col - i] += 1;
-		}else{
-		    board[row + i][col + i] += 1;
-		}
+	    if((i - col + row >= 0) && (i - col + row < board.length) && (i != col)){
+		board[i][i - col + row] += 1;
+	    }
+	    if ((i != col) &&  ((-1*i) + col + row >= 0) && ((-1*i) + row + col < board.length)) {
+		board[i][(-1*i) + row + col] += 1;
 	    }
 	}
     }
 
+
     //Removes the queen in row,column and removes all of the danger markers it added to the board.
-    private void removeQueen(int row,int col){
-	for(int i = 0;i < board.length;i++){
-	    board[row][i] -= 1;
-	    board[i][col] -= 1;
-	    if(i < row){
-		if(i < col){
-		    board[row - i][col - i] -= 1;
-		}else{
-		    board[row - i][col + i] -= 1;
-		}
+    private void removeQueen(int col,int row){
+	board[col][row] += 1;
+	for(int i = 0; i<board.length;i++){
+	    if(i != col){
+		board[i][row] -= 1;
 	    }
-	    if(i > row){
-		if(i < col){
-		    board[row + i][col - i] -= 1;
-		}else{
-		    board[row + i][col + i] -= 1;
-		}
+	    if((i - col + row >= 0) && (i - col + row < board.length) && (i != col)){
+		board[i][i - col + row] -= 1;
 	    }
-	    board[row][col] == 0;
+	    if ((i != row) &&  ((-1*i) + row + col >= 0) && ((-1*i) + row + col < board.length)) {
+		board[i][(-1*i) + row + col] -= 1;
+	    }
 	}
     }
 
@@ -90,22 +82,45 @@ public class QueenBoard{
 	return -1;
     }
 
+    public String debug(){
+	String layout = "";
+	for(int i = 0;i<board.length;i++){
+	    for(int k = 0;k<board.length;k++){
+		if(board[i][k] == -1){
+		    layout += "Q ";
+		}else{
+		    layout += board[i][k];
+		}
+		if(k == board.length - 1){
+		    layout += "\n";
+		}
+	    }
+	}
+	return layout;
+    }
+	
     public String toString(){
-	String layout = "[ ";
-	for(int i = 0;i<board.size;i++){
-	    for(int k = 0;k<board.size;k++){
+	String layout = "";
+	for(int i = 0;i<board.length;i++){
+	    for(int k = 0;k<board.length;k++){
 		if(board[i][k] == -1){
 		    layout += "Q ";
 		}
 		if(board[i][k] == 0 || board[i][k] >= 1){
 		    layout += "_ ";
 		}
-		if(i != 0 && k == 0){
-		    layout += "[ ";
-		}
-		if(k == 7){
-		    layout += " ]";
+		if(k == board.length - 1){
+		    layout += "\n";
 		}
 	    }
 	}
+	return layout;
     }
+
+    public static void main(String[]args){
+	QueenBoard b = new QueenBoard(4);
+	b.solve();
+	System.out.println(b.toString());
+	System.out.println(b.debug());
+    }
+}

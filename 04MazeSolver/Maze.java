8dd2e430675d94e8b301e7adbdcbc1f22a2135b3
soe43 <soe433@gmail.com>
2017-03-02 14:49:57 -0500

    Fixed reading of maze from scanner into char array.

--- 04MazeSolver/Maze.java
+++ 04MazeSolver/Maze.java
@@ -1,9 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Maze{
     private char[][] maze;
+    private String mazeString;
+    private int row = 0;
+    private int col = 0;
     private boolean animate; 
     private int startx = 0;
     private int starty = 0;
@@ -16,14 +20,15 @@ public class Maze{
 	}catch(FileNotFoundException e){
 	    System.out.println("File Not Found.");
 	    System.exit(1);
-	}	    
+	}
+	maze = new char[row][col];
 	for(int i = 0;i <  maze.length;i++){
 	    for(int k = 0;k < maze[0].length;k++){
-		if(maze[i][k] = 'S' || maze[i][k] == 's'){
+		if(maze[i][k] == 'S' || maze[i][k] == 's'){
 		    startx = i;
 		    starty = k;
 		}
-		if(maze[i][k] = 'E' || maze[i][k] == 'e'){
+		if(maze[i][k] == 'E' || maze[i][k] == 'e'){
 		    endx = i;
 		    endy = k;
 		}
@@ -34,13 +39,43 @@ public class Maze{
     private void readFile(String inputF) throws FileNotFoundException{
 	File inputFile = new File(inputF);
 	Scanner inf = new Scanner(inputFile);
-	while(inf.hasNext()){
-	    maze += inf.next();
+	Scanner infdim = new Scanner(inputFile);
+	while(infdim.hasNextLine()){
+	    col = infdim.nextLine().length();
+	    row++;
 	}
+	System.out.println(row+" "+col);
+	maze = new char[row][col];
+	try{
+	    for(int i = 0;i < row;i++){
+		for(int k = 0;k < col;k++){
+		    maze[i][k] = inf.next().charAt(0);
+		}
+	    }
+	}catch(NoSuchElementException e){
+	    return;
+	}
+	/*
+	while(inf.hasNext()){
+	    mazeString += inf.next(); 
+	}
+	System.out.println("done");
+	System.out.println(mazeString);
+	Scanner t = new Scanner(mazeString);
+	Scanner f = new Scanner(mazeString);
+	t.useDelimiter("/n");
+	while(t.hasNextLine()){
+	    col = col + 1;
+	}
+	System.out.println("done");
+	row = f.nextLine().length();
+	System.out.println("done");
+	*/
     }
 
     public boolean solve(){
 	solveH(0,0);
+	return false;
     }
 
     private boolean solveH(int startX,int startY){

0ce39e7eb239da6e6dd8d71e2a11fe9585d67e2c
soe43 <soe433@gmail.com>
2017-04-03 22:04:45 -0400

    Finished remove, add, get, set, and some peripheral methods. Will create battery of tests very soon.

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -1,17 +1,49 @@
 public class MyLinkedList{
     private LNode start;
+    private LNode end;
     private int size;
 
     public MyLinkedList(){
 	start = null;
+	end = null;
 	size = 0;
     }
     
-    public boolean addStart(int value){
-	LNode a = new LNode(value,start);
-	start = a;
-	size++;
-	return true;
+    public boolean add(int index, int value){
+	if(index < 0 || index > size+1){
+	    return false;
+	}
+	if(size == 0){
+	    LNode a = new LNode(value);
+	    start = a;
+	    end = a;
+	    size++;
+	    return true;
+	}
+	if(index == 0){
+	    LNode a = new LNode(value,start,null);
+	    start.prev = a;
+	    start = a;
+	    size++;
+	    return true;
+	}
+	if(index == size+1){
+	    LNode a = new LNode(value,null,getNthNode(index));
+	    end.prev = a;
+	    end = a;
+	    size++;
+	    return true;
+	}
+	else{
+	    LNode a = new LNode(value);
+	    LNode b = getNthNode(index);
+	    a.prev = b;
+	    a.next = b.next;
+	    b.next = a;
+	    b.next.prev = a;
+	    size++;
+	    return true;
+	}   
     }
 
     public int size(){
@@ -35,16 +67,46 @@ public class MyLinkedList{
     public int set(int index,int newValue){
 	LNode a = getNthNode(index);
 	int temp = a.value;
-	getNthNode.value = newValue;
+	getNthNode(index).value = newValue;
 	return temp;
     }	
 
+    private int remove(LNode a){
+	int temp = a.value;
+	if(size == 0){
+	    throw new IllegalArgumentException();
+	}
+	if(size == 1){
+	    start = null;
+	    end = null;
+	    size--;
+	}
+	if(a.prev == null){
+	    start = a.next;
+	    a.next.prev = null;
+	    size--;;
+	}
+	if(a.next == null){
+	    end = a.prev;
+	    a.prev.next = null;
+	    size--;
+	}
+	else{
+	int val = a.value;
+	a.prev.next = a.next;
+	a.next.prev = a.prev;
+	size--;
+	}
+	return temp;
+    }
+
+	
     private LNode getNthNode(int n){
 	if(n >= size() || n < 0){
 	    throw new IndexOutOfBoundsException();
 	}
 	LNode a = start;
-	for(int i = 0;i < n;i++){
+	while(n >= 0){
 	    a = a.next;
 	}
 	return a;
@@ -55,15 +117,18 @@ public class MyLinkedList{
     private class LNode{
 	public int value;
 	private LNode next;
+	private LNode prev;
 	
 	public LNode(int input){
 	    value = input;
 	    next = null;
+	    prev = null;
 	}
 
-	public LNode(int input, LNode next){
+	public LNode(int input, LNode next, LNode prev){
 	    value = input;
 	    this.next = next;
+	    this.prev = prev;
 	}
     }
 }

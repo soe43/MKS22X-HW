6597a409088edac904d12b17725a9118f54847ee
soe43 <soe433@gmail.com>
2017-04-21 14:51:27 -0400

    q work on the way to completion

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,48 +1,26 @@
 import java.util.*;
 
 public class Quick{
-    /*
-    private static int part(int[] data, int start, int end){
-	int left = start;
-	int right = end-1;
-	Random r = new Random();
-	int pivot = start + r.nextInt(end - start);
-	int pivotValue = data[pivot];
-	switchPos(data,pivot,left);
-	left++;
-	int i = left;
-        while(i < right){
-	    if(data[i] < pivotValue){
-		left++;
-		i++;
-	    }
-	    if(data[i] > pivotValue){
-		switchPos(data,i,right);
-		right--;
-	    }
-	    else{
-		i++;
-	    }
-	}
-        switchPos(data,0,right);
-	return left;
-    }
-    */
 
+    private static int part(int[]data){
+	return part(data,0,data.length);
+    }
+    
     private static int part(int[] data, int start, int end){
-	int left = start;
-	int right = end-1;
 	Random r = new Random();
 	int pivot = start + r.nextInt(end - start);
 	int pivVal = data[pivot];
-	switchPos(data, left, pivot); 
+	System.out.println(pivot+","+pivVal);
+	switchPos(data,pivot,start);
+	int left = start + 1;
+	int right = end-1;
 	int i = left;
-	while(i <= right){
+	while(left < right){
 	    if(data[i] < pivVal){
 		left++;
 		i++;
 	    }
-	    if(data[i] > pivVal){
+	    else if(data[i] > pivVal){
 		switchPos(data,i,right);
 		right--;
 	    }
@@ -51,13 +29,22 @@ public class Quick{
 	    }
 	}
 	switchPos(data,0,right);
-	return right;
+	System.out.println(i);
+	return i;
     }
-	
+
     private static void switchPos(int[] ary, int position1, int position2){
 	int temp = ary[position1];
 	ary[position1] = ary[position2];
 	ary[position2] = temp;
+    }
+
+    public static int quickselect(int[] data, int k){
+	return quickselectH(data,k,0,data.length);
+    }
+
+    public static int quickselectH(int[] data, int k, int start, int end){
+	return 0;
     }
 
     public static String toString(int[] toPrint){
@@ -73,82 +60,19 @@ public class Quick{
 	return layout;
     }
 
-    public static int quickselect(int[] data, int k){
-	return quickselectH(data,k,0,data.length);
-    }
-    
-    private static int quickselectH(int[] data, int k,int start,int end){
-	while(end > start){
-	    int pivot = part(data,start,end);
-	    if(k == pivot){
-		return data[k];
-	    }
-	    else if(k > pivot){
-		return quickselectH(data,k,pivot+1,end);
-	    } else {
-		return quickselectH(data,k,start,pivot-1);
-	    }
-	}
-	return data[k];
-    }
-
-    public static void quickSort(int[] ary){
-	//quickSortH(ary,0,ary.length);
-    }
-
-    public static void quickSortH(int[] ary, int left, int right){
-			      
-    }
-
-    public static void main(String[] args){
+    public static void main(String[]args){
 	Random r = new Random();
-	int[][] arys = new int[5][8];
-	for(int i = 0;i < 5;i++){
+	int[][] arys = new int[100][8];
+	for(int i = 0;i < 100;i++){
 	    for(int k = 0;k < 8;k++){
 		arys[i][k] = r.nextInt(50);
 	    }
 	}
-	for(int i = 0; i < 5; i++){
+	for(int i = 0;i < 100; i++){
 	    System.out.println(toString(arys[i]));
-	    System.out.println("1st Smallest: " + quickselect(arys[i],1));
+	    part(arys[i]);
 	    System.out.println(toString(arys[i]));
-	    System.out.println();	    
-	}
-	int[] ary = {999,0,999,1,2,999,4,6};
-	for(int i = 0; i < ary.length;i++){
-	    if(i == 1){
-		System.out.println(""+i+"st Smallest: " +quickselect(ary,i));
-	    }
-	    else if(i == 2){
-		System.out.println(""+i+"nd Smallest: " +quickselect(ary,i));
-	    }
-	    else if(i == 3){
-		System.out.println(""+i+"rd Smallest: " +quickselect(ary,i));
-	    }
-	    else{
-		System.out.println(""+i+"th Smallest: " +quickselect(ary,i));
-	    }
-	}
-	
-	int[] scrambled = {5,6,3,1,8,0};
-	System.out.println();
-	System.out.println(toString(scrambled));
-	quickSort(scrambled);
-	System.out.println("Sorted: \n" + toString(scrambled)); 
-	System.out.println();
-
-	int[][] scrambledArys = new int[10][10];
-	for(int i = 0;i < 10;i++){
-	    for(int k = 0;k < 10;k++){
-		scrambledArys[i][k] = r.nextInt(10);
-	    }
-	}
-	for(int i = 0; i < 10; i++){
-	    System.out.println(toString(scrambledArys[i]));
-	    quickSort(scrambledArys[i]);
-	    System.out.println("Sorted: \n" + toString(scrambledArys[i]));
-	    System.out.println();	    
+	    System.out.println("");
 	}
     }
-   
 }
---
+++ 07Quick/QuickDev.java~
@@ -0,0 +1,154 @@
+import java.util.*;
+
+public class Quick{
+    /*
+    private static int part(int[] data, int start, int end){
+	int left = start;
+	int right = end-1;
+	Random r = new Random();
+	int pivot = start + r.nextInt(end - start);
+	int pivotValue = data[pivot];
+	switchPos(data,pivot,left);
+	left++;
+	int i = left;
+        while(i < right){
+	    if(data[i] < pivotValue){
+		left++;
+		i++;
+	    }
+	    if(data[i] > pivotValue){
+		switchPos(data,i,right);
+		right--;
+	    }
+	    else{
+		i++;
+	    }
+	}
+        switchPos(data,0,right);
+	return left;
+    }
+    */
+
+    private static int part(int[] data, int start, int end){
+	int left = start;
+	int right = end-1;
+	Random r = new Random();
+	int pivot = start + r.nextInt(end - start);
+	int pivVal = data[pivot];
+	switchPos(data, left, pivot); 
+	int i = left;
+	while(i <= right){
+	    if(data[i] < pivVal){
+		left++;
+		i++;
+	    }
+	    if(data[i] > pivVal){
+		switchPos(data,i,right);
+		right--;
+	    }
+	    else{
+		i++;
+	    }
+	}
+	switchPos(data,0,right);
+	return right;
+    }
+	
+    private static void switchPos(int[] ary, int position1, int position2){
+	int temp = ary[position1];
+	ary[position1] = ary[position2];
+	ary[position2] = temp;
+    }
+
+    public static String toString(int[] toPrint){
+	String layout = "[";
+	for(int i = 0; i < toPrint.length;i++){
+	    if(i == toPrint.length - 1){
+		layout += toPrint[i];
+	    }else{
+		layout += toPrint[i] +", ";
+	    }
+	}
+	layout+="]";
+	return layout;
+    }
+
+    public static int quickselect(int[] data, int k){
+	return quickselectH(data,k,0,data.length);
+    }
+    
+    private static int quickselectH(int[] data, int k,int start,int end){
+	while(end > start){
+	    int pivot = part(data,start,end);
+	    if(k == pivot){
+		return data[k];
+	    }
+	    else if(k > pivot){
+		return quickselectH(data,k,pivot+1,end);
+	    } else {
+		return quickselectH(data,k,start,pivot-1);
+	    }
+	}
+	return data[k];
+    }
+
+    public static void quickSort(int[] ary){
+	//quickSortH(ary,0,ary.length);
+    }
+
+    public static void quickSortH(int[] ary, int left, int right){
+			      
+    }
+
+    public static void main(String[] args){
+	Random r = new Random();
+	int[][] arys = new int[5][8];
+	for(int i = 0;i < 5;i++){
+	    for(int k = 0;k < 8;k++){
+		arys[i][k] = r.nextInt(50);
+	    }
+	}
+	for(int i = 0; i < 5; i++){
+	    System.out.println(toString(arys[i]));
+	    System.out.println("1st Smallest: " + quickselect(arys[i],1));
+	    System.out.println(toString(arys[i]));
+	    System.out.println();	    
+	}
+	int[] ary = {999,0,999,1,2,999,4,6};
+	for(int i = 0; i < ary.length;i++){
+	    if(i == 1){
+		System.out.println(""+i+"st Smallest: " +quickselect(ary,i));
+	    }
+	    else if(i == 2){
+		System.out.println(""+i+"nd Smallest: " +quickselect(ary,i));
+	    }
+	    else if(i == 3){
+		System.out.println(""+i+"rd Smallest: " +quickselect(ary,i));
+	    }
+	    else{
+		System.out.println(""+i+"th Smallest: " +quickselect(ary,i));
+	    }
+	}
+	
+	int[] scrambled = {5,6,3,1,8,0};
+	System.out.println();
+	System.out.println(toString(scrambled));
+	quickSort(scrambled);
+	System.out.println("Sorted: \n" + toString(scrambled)); 
+	System.out.println();
+
+	int[][] scrambledArys = new int[10][10];
+	for(int i = 0;i < 10;i++){
+	    for(int k = 0;k < 10;k++){
+		scrambledArys[i][k] = r.nextInt(10);
+	    }
+	}
+	for(int i = 0; i < 10; i++){
+	    System.out.println(toString(scrambledArys[i]));
+	    quickSort(scrambledArys[i]);
+	    System.out.println("Sorted: \n" + toString(scrambledArys[i]));
+	    System.out.println();	    
+	}
+    }
+   
+}

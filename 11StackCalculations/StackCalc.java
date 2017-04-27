781eb7be62b16f93b3d6de3c0180cae2ed0405c9
soe43 <soe433@gmail.com>
2017-04-27 14:49:36 -0400

    Fixed stackCalc and worked on ExpressionTree

--- 11StackCalculations/StackCalc.java
+++ 11StackCalculations/StackCalc.java
@@ -5,9 +5,6 @@ public static double eval(String s){
     String[] tokens = s.split(" ");
     Stack<Double> values = new Stack<Double>();
 
-    if(tokens.length == 1){
-	return Double.parseDouble(tokens[0]);
-    }
     for(String token : tokens){
 	if(isOp(token)){
 	    values.push(perform(token,values.pop(),values.pop()));
@@ -20,12 +17,13 @@ public static double eval(String s){
 }
 
 private static boolean isOp(String s){
-    switch(s){
-    case "+": return true;
-    case "-": return true;
-    case "*": return true;
-    case "/": return true;
-    case "%": return true;
+    char ch = s.charAt(0);
+    switch(ch){
+    case '+': return true;
+    case '-': return true;
+    case '*': return true;
+    case '/': return true;
+    case '%': return true;
     default: return false;
     }
 }
@@ -33,23 +31,32 @@ private static boolean isOp(String s){
 private static double perform(String op,double n1,double n2){
     double sol = 0;
     if(op.equals("+")){
-	sol = n1 + n2;
+	sol = n2 + n1;
     }
     if(op.equals("-")){
-	sol = n1-n2;
+	sol = n2 - n1;
     }
     if(op.equals("/")){
-	sol = n1/n2;
+	sol = n2/n1;
      }
     if(op.equals("*")){
-	sol = n1*n2;
+	sol = n2*n1;
     }
     if(op.equals("%")){
-	sol = n1%n2;
+	sol = n2%n1;
     }
     return sol;
 }
 
+public static void main(String[] args)
+{
+    System.out.println(StackCalc.eval("10 2 +"));//12.0
+    System.out.println(StackCalc.eval("10 2 -"));//8.0
+    System.out.println(StackCalc.eval("10 2.0 +"));//12.0
+    System.out.println(StackCalc.eval("11 3 - 4 + 2.5 *"));//30.0
+    System.out.println(StackCalc.eval("8 2 + 99 9 - * 2 + 9 -"));//893.0
+    System.out.println(StackCalc.eval("10 2 + 10 * 1 + 1 1 1 + + + 10 10 + -"));//104.0
+}
 }
 	
        

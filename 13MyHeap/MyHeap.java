e44d9a8abbe998385b3b6eaad7abfcacf5205e93
soe43 <soe433@gmail.com>
2017-05-03 14:51:25 -0400

    Started the heap class from backbone

---
+++ 13MyHeap/MyHeap.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class MyHeap{
+    private ArrayList<String> heaparino;
+
+    public MyHeap(){ // construct empty max heap
+    }
+    
+    // true: construct empty max heap, false: construct empty min heap.
+    public MyHeap(boolean maxmin){  
+	if(maxmin){
+	    //make max-heap
+	}
+	else{
+	    //make min-heap
+	}
+   }
+ 
+    
+    //Public Methods
+    public void add(String s){}
+    
+    public String remove(){}
+    
+    public String peek(){}
+
+    //Private Methods
+    private String rChild(/*parent*/){}
+
+    private String lChild(/*parent*/){}
+
+ 
